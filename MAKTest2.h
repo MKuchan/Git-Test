c206c1439bd1bc278fa6d95a046ee68ffde6a49d
MKuchan <user23807@MAC328.local>
2014-07-31 16:34:56 -0400

    Added Test2

---
+++ MAKTest2.h
@@ -0,0 +1,13 @@
+//
+//  MAKTest2.h
+//  Git Test
+//
+//  Created by user23807 on 7/31/14.
+//  Copyright (c) 2014 MAKuchanApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAKTest2 : NSObject
+
+@end
---
+++ MAKTest2.m
@@ -0,0 +1,13 @@
+//
+//  MAKTest2.m
+//  Git Test
+//
+//  Created by user23807 on 7/31/14.
+//  Copyright (c) 2014 MAKuchanApps. All rights reserved.
+//
+
+#import "MAKTest2.h"
+
+@implementation MAKTest2
+
+@end
